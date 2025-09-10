fc9a9db7b1b48a04d04ace674872c57c3fbf4b13
SHLcomp <salibahe123@gmail.com>
2025-09-10 18:18:50 +0300

    Fix filename capitalization for Vercel build

---
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -0,0 +1,28 @@
+import "./ContactInfo.scss";
+import data from "../../data/contactInfo";
+
+export const ContactInfo = () => {
+  return (
+    <div className="contactInfo" id="contact">
+        <h2>معلومات التواصل</h2>
+      <div className="contact-cont">
+        {data.map((item) => {
+          return (
+            <div className="item">
+              <div className="txt">
+                <h4>{item.title}</h4>
+                <i className={item.icon}></i>
+              </div>
+              <ul>
+                {item.des.map((item, index)=> <li key={index}>{item}</li>)}
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+              <h4 className="cta"> <i className="fa-brands fa-whatsapp fa-xl"></i>تواصل عن طريق الواتس</h4>
+
+    </div>
+  );
+};
+export default ContactInfo;
